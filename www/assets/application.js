12ac62380fadf6c9c92d0b8fbbfc3988f5e15e9a
Mohaballi Barakat <mohaballi@mnb.local>
2013-12-18 15:10:31 -0500

    Compile assets

--- www/assets/application.js
+++ www/assets/application.js
@@ -2981,56 +2981,32 @@ var shake = (function () {
 
 
 
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-        deviceReady();
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = $(id);
-        var listeningElement = $('.listening');
-        var receivedElement = $('.received');
+$(function(){
+  document.addEventListener("deviceready", onDeviceReady, false);
+});
 
-        listeningElement.attr('style', 'display:none;');
-        receivedElement.attr('style', 'display:inline-block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
-
-function deviceReady(){
-  $(function() {
-    // Load Menu
-    $('#menu-placeholder').load("pages/menu.html", function(){
-      bindPageLinks();
-    });
+function onDeviceReady(){
+  // Listen for backbutton
+  document.addEventListener("backbutton", onBackKeyDown, false);
   
-    // Workaround for navbar bug on orientation change
-    $(window).bind('resize', function() {  
-      $('#main-navbar').css('width', $('body').css('width'));
-    }).trigger('resize');
-    
-    // Eliminate the 300ms delay after physical taps
-    FastClick.attach(document.body);
-    
+  // Load Menu
+  $('#menu-placeholder').load("pages/menu.html", function(){
     bindPageLinks();
   });
+
+  // Workaround for navbar bug on orientation change
+  $(window).bind('resize', function() {  
+    $('#main-navbar').css('width', $('body').css('width'));
+  }).trigger('resize');
+  
+  // Eliminate the 300ms delay after physical taps
+  FastClick.attach(document.body);
+  
+  bindPageLinks();
+}
+
+function onBackKeyDown(){
+  $('.navbar-brand.page-link').trigger('click');
 }
 
 function bindPageLinks(){
