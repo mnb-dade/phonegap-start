d6cde26036e18c9c5decc553dbac9e55f77a4519
Mohaballi Barakat <mohaballi@mnb.local>
2013-12-18 15:43:38 -0500

    Capture API - testing audio capture

---
+++ assets/javascripts/capture.js
@@ -0,0 +1,28 @@
+function captureAudio() {
+  // Launch device audio recording application,
+  // allowing user to capture an audio clip
+  navigator.device.capture.captureAudio(captureSuccess, captureError, { limit: 1 });
+}
+
+// Called when capture operation is finished
+//
+function captureSuccess(mediaFiles) {
+  var i, len;
+  for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+    displayFile(mediaFiles[i], 'Audio');
+  }
+}
+
+// Upload files to server
+function displayFile(mediaFile, mediaType) {
+  var name = mediaFile.name;
+  var output = $('#capture-output');
+  output.append('<li>' + mediaType + ': ' + name + '</li>');
+}
+
+// Called if something bad happens.
+//
+function captureError(error) {
+  var msg = 'An error occurred during capture: ' + error.code;
+  navigator.notification.alert(msg, null, 'Uh oh!');
+}
--- www/assets/application.js
+++ www/assets/application.js
@@ -2108,6 +2108,35 @@ function onFail(message) {
   alert('Failed because: ' + message);
 }
 ;
+function captureAudio() {
+  // Launch device audio recording application,
+  // allowing user to capture an audio clip
+  navigator.device.capture.captureAudio(captureSuccess, captureError, { limit: 1 });
+}
+
+// Called when capture operation is finished
+//
+function captureSuccess(mediaFiles) {
+  var i, len;
+  for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+    displayFile(mediaFiles[i], 'Audio');
+  }
+}
+
+// Upload files to server
+function displayFile(mediaFile, mediaType) {
+  var name = mediaFile.name;
+  var output = $('#capture-output');
+  output.append('<li>' + mediaType + ': ' + name + '</li>');
+}
+
+// Called if something bad happens.
+//
+function captureError(error) {
+  var msg = 'An error occurred during capture: ' + error.code;
+  navigator.notification.alert(msg, null, 'Uh oh!');
+}
+;
 /**
  * @preserve FastClick: polyfill to remove click delays on browsers with touch UIs.
  *
