07ce7a76ae4dd43cb8d53764fc157b31fe297f51
Mohaballi Barakat <mohaballi@mnb.local>
2013-12-20 12:06:20 -0500

    Use accelerometer to fly a plane

--- assets/javascripts/accelerometer.js
+++ assets/javascripts/accelerometer.js
@@ -4,6 +4,20 @@ function accelerometerPageLoaded(){
   // Update acceleration every 3 seconds
   var options = { frequency: 100 };
   watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+  
+  window.addEventListener('deviceorientation', function (event) {
+    var alpha = event.alpha;
+    var beta = event.beta;
+    var plane = $('#plane');
+    
+    plane.css('-transform', 'rotate(' + -alpha + 'deg)');
+    plane.css('-webkit-transform', 'rotate(' + -alpha + 'deg)');
+    plane.css('-ms-transform', 'rotate(' + -alpha + 'deg)');
+    
+    if(beta < 0){
+      plane.css('width', 100 + beta + '%');
+    }
+  });
 }
 
 function onSuccess(acceleration){
