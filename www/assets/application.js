ed1bb3473657484aac1a0aa7f963a5779694558b
Mohaballi Barakat <mohaballi@mnb.local>
2013-12-20 15:11:30 -0500

    Use accel. to fly plane

--- www/assets/application.js
+++ www/assets/application.js
@@ -2013,41 +2013,30 @@ u[o]&&(delete u[o],c?delete n[l]:typeof n.removeAttribute!==i?n.removeAttribute(
 var watchID = null;
 
 function accelerometerPageLoaded(){
-  // Update acceleration every 3 seconds
   var options = { frequency: 100 };
   watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
-  
-  // window.addEventListener('deviceorientation', function (event) {
-  //   var alpha = event.alpha;
-  //   var beta = event.beta;
-  //   var gamma = event.gamma;
-  //   
-  //   var alpha_offset = 270;
-  //   var beta_offset = 90;
-  //   
-  //   var alpha_rotation = alpha_offset - alpha;
-  //   
-  //   var plane = $('#plane');
-  //   
-  //   plane.css('-transform', 'rotate(' + alpha_rotation + 'deg)');
-  //   plane.css('-webkit-transform', 'rotate(' + alpha_rotation + 'deg)');
-  //   plane.css('-ms-transform', 'rotate(' + alpha_rotation + 'deg)');
-  //   
-  //   if(beta < beta_offset){
-  //     plane.css('width', 100 - (beta_offset - beta) + '%');
-  //   }
-  //   
-  //   $('#output').html('Alpha: ' + alpha + '<br />' +
-  //                     'Beta: ' + beta + '<br />' +
-  //                     'Gamma: ' + gamma + '<br />')
-  // });
 }
 
 function onSuccess(acceleration){
-  $('#output').html('Acceleration X: ' + acceleration.x         + '<br />' +
-                    'Acceleration Y: ' + acceleration.y         + '<br />' +
-                    'Acceleration Z: ' + acceleration.z         + '<br />' +
-                    'Timestamp: '      + acceleration.timestamp + '<br />')
+  var output = $('#output');
+  var plane = $('#plane');
+  var x = acceleration.x;
+  var y = acceleration.y;
+  var z = acceleration.z;
+  var x_rotation = -(x * 2);
+  
+  output.html('Acceleration X: ' + acceleration.x         + '<br />' +
+              'Acceleration Y: ' + acceleration.y         + '<br />' +
+              'Acceleration Z: ' + acceleration.z         + '<br />' +
+              'Timestamp: '      + acceleration.timestamp + '<br />');
+                    
+  plane.css('-transform', 'rotate(' + x_rotation + 'deg)');
+  plane.css('-webkit-transform', 'rotate(' + x_rotation + 'deg)');
+  plane.css('-ms-transform', 'rotate(' + x_rotation + 'deg)');
+  
+  if(z > 4 && z < 9){
+    plane.css('width', 100 - (z * 10) + '%');
+  }
 }
 
 function onError(){
